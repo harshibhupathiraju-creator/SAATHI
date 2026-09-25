345f51577fe14e8eeca71cd91f628c292822932e
harshini bhupathiraju <harshi.bhupathiraju@gmail.com>
2026-09-26 00:03:53 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,50 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 8000;
+const DIR = __dirname;
+
+const server = http.createServer((req, res) => {
+  let safePath = req.url.split('?')[0];
+  if (safePath === '/' || safePath === '') safePath = '/index.html';
+  const filePath = path.join(DIR, safePath);
+
+  const ext = path.extname(filePath).toLowerCase();
+  const mimeTypes = {
+    '.html': 'text/html; charset=utf-8',
+    '.htm': 'text/html; charset=utf-8',
+    '.js': 'text/javascript; charset=utf-8',
+    '.css': 'text/css; charset=utf-8',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.svg': 'image/svg+xml',
+    '.ico': 'image/x-icon'
+  };
+
+  const contentType = mimeTypes[ext] || 'application/octet-stream';
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found: ' + safePath);
+      } else {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('500 Server Error: ' + err.code);
+      }
+    } else {
+      res.writeHead(200, { 
+        'Content-Type': contentType,
+        'Access-Control-Allow-Origin': '*'
+      });
+      res.end(content);
+    }
+  });
+});
+
+server.listen(PORT, '127.0.0.1', () => {
+  console.log(`Saathi Telemetry Server running at http://localhost:${PORT}/`);
+  console.log(`WebSerial API is fully enabled in Google Chrome on localhost!`);
+});
